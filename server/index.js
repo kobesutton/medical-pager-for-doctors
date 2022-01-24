1662f8f6a30fdb13df5b024ba6f0eff59f08c89e
kobesutton <85741944+kobesutton@users.noreply.github.com>
2022-01-23 20:49:46 -0500

    first commit

---
+++ server/index.js
@@ -0,0 +1,73 @@
+const express = require('express')
+const cors = require('cors')
+
+const authRoutes = require("./routes/auth.js")
+
+const app = express()
+
+const PORT = process.env.PORT || 5000
+
+require('dotenv').config()
+const accountSid = process.env.TWILIO_ACCOUNT_SID
+const authToken = process.env.TWILIO_AUTH_TOKEN
+const twilioClient = require('twilio')(accountSid, authToken)
+const messagingServiceSid = process.env.TWILIO_MESSAGING_SERVICE_SID
+
+//middleware
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.post('/', (req, res) => {
+    const { message, user: sender, type, members } = req.body
+
+    if(type === 'message.new') {
+        members
+        .filter((member) => member.user_id  !== sender.id)
+        .forEach(({ user }) => {
+            if(!user.online) {
+                twilioClient.messages.create({
+                    body: `You have a new message from ${message.user.fullName} - ${message.text}`,
+                    messagingServiceSid,
+                    to: user.phoneNumber
+                })
+
+                .then(() => console.log('Message Sent!'))
+                .catch((err) => console.log(err))
+            }
+        })
+        return res.status(200).send('Message sent!')
+    }
+    return res.status(200).send('Not a new message request')
+
+})
+
+app.use('/auth', authRoutes)
+
+//error handler 
+
+app.use((req, res, next) => {
+
+    const error = new Error('Not found')
+    error.status = 404
+    next(error)
+})
+
+
+app.use((error, req, res, next) => {
+
+    res.status(error.status || 500)
+    res.send({
+        error: {
+            status: error.status || 500,
+            message: error.message 
+        }
+    })
+})
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
